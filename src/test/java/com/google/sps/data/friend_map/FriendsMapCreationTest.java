c20f27b866ab9acabc505274feb12ab89a10a3a4
Isabelle Dittamo <mivdittamo@gmail.com>
2020-07-01 14:29:44 +0000

    changed friend_graph directory to friend_map

--- src/test/java/com/google/sps/data/friend_map/FriendsMapCreationTest.java
+++ src/test/java/com/google/sps/data/friend_map/FriendsMapCreationTest.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.sps.data.friend_graph;
+package com.google.sps.data.friend_map;
 
 import java.util.Set;
 import java.util.Map;
