371a6208e61a0e4e13babc1955700ada1afc22d4
Jess DeJong <jessdejong843@gmail.com>
2020-07-01 19:31:05 +0000

    Separate facebook login snippet into two lines

--- src/main/webapp/facebook-login.js
+++ src/main/webapp/facebook-login.js
@@ -45,7 +45,8 @@ window.fbAsyncInit = function() {
 (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
+  js = d.createElement(s);
+  js.id = id;
   js.src = "https://connect.facebook.net/en_US/sdk.js";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
