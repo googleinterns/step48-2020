ab3b8fb9eb8314d6c17d7da2bee103bd54d9f8e1
Jess DeJong <jessdejong843@gmail.com>
2020-07-06 21:54:37 +0000

    Separate facebook SDK load into separate file

--- src/main/webapp/facebook-login.js
+++ src/main/webapp/facebook-login.js
@@ -22,34 +22,7 @@ function include(javascriptFile) {
   document.getElementsByTagName('head').item(0).appendChild(script); 
 } 
 include('./FacebookAPI.js');
-
-// Async initialization of Facebook Javascript SDK
-window.fbAsyncInit = function() {
-  FB.init({
-    // Connect Javascript SDK to the Friends of Friends Facebook App
-    appId      : '313886196298676',
-    // Parse social plugins on this webpage
-    xfbml      : true,
-    // Use the most recent version of the Facebook Graph API
-    version    : 'v7.0'
-  });
-
-  // Returns the user login 'status'
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-};
-
-// Async load of Facebook Javascript SDK
-// Provided by Facebook to load Javascript SDK
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://connect.facebook.net/en_US/sdk.js";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+include('./load-facebook.js');
 
 // Response to the user's login status, redirect appropriately
 function statusChangeCallback(response) {
