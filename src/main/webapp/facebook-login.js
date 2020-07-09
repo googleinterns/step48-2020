6b4ee9d2c71ec3ef09fb6631fe7dede2f853ebb6
Jess DeJong <jessdejong843@gmail.com>
2020-07-09 14:22:37 +0000

    Create post request to store user information on user login

--- src/main/webapp/facebook-login.js
+++ src/main/webapp/facebook-login.js
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+ 
 // Javascript 'import' statements are not supported by browsers
 // Instead, inject a script tag
 function include(javascriptFile) { 
@@ -21,9 +21,8 @@ function include(javascriptFile) {
   script.defer = true; 
   document.getElementsByTagName('head').item(0).appendChild(script); 
 } 
-include('./FacebookAPI.js');
 include('./load-facebook.js');
-
+ 
 // Response to the user's login status, redirect appropriately
 function statusChangeCallback(response) {
   if (response.status === 'connected') {
@@ -31,25 +30,31 @@ function statusChangeCallback(response) {
     // TODO: Redirect user to the correct page depending on whether their profile is complete
     // For now, redirect to the profile page
     // Query Facebook Graph API for user information
-    FacebookAPI.getUserFacebookInfoAndRedirect(
-      function(response) {
-        let userInfo = new URLSearchParams({
-          name: response.name,
-          id: response.id,
-          email: response.email,
-        });
-
-        // Store user information in Datastore
-        // TODO: Add post request
-        // Redirect to the profile page, and pass the user's info in the URL
+    FB.api('/me?fields=id,name,email,friends', function(response) {
+      let userInfo = new URLSearchParams({
+        name: response.name,
+        id: response.id,
+        email: response.email,
+      });
+ 
+      // Add friend ids to the query string
+      // TODO: (After MVP) Replace this with jquery + ajax to POST JSON to the backend
+      response.friends.data.forEach((friend) => userInfo.append("friends-list", friend.id));
+ 
+      // Store user information in Datastore
+      fetch('/user-data?' + userInfo.toString(), {
+        method: 'POST',
+      });
+ 
+      // Redirect to the profile page, and pass the user's info in the URL
       window.location.href = "./profile.html?" + userInfo.toString();
     });
   }
 }
-
+ 
 // Called after the user logs in with Facebook
 function checkLoginState() {
-  FacebookAPI.checkLoginStatus(function(response) {
+  FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
   });
 }
