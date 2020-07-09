ad478853c19521bb2b0f0880e2aa5c3da8a3d858
Alisha Kewalramani <alikew24@gmail.com>
2020-07-09 19:50:46 +0000

    add nits from pr, tried mutation observer but it didn't work out

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -36,14 +36,7 @@ function updateMatchesLinkForCurrentUser() {
   link.setAttribute('href', 'matches.html?id=' + uid);
 }
 
-function loadAllUpdatedLinks() {
-    updateProfileLinkForCurrentUser();
-    updateFeedLinkForCurrentUser();
-    updateMatchesLinkForCurrentUser();
-}
 window.addEventListener("load", function(){
-   updateProfileLinkForCurrentUser();
-    updateFeedLinkForCurrentUser();
-    updateMatchesLinkForCurrentUser();
+  function a(a,b){var c=/^(?:file):/,d=new XMLHttpRequest,e=0;d.onreadystatechange=function(){4==d.readyState&&(e=d.status),c.test(location.href)&&d.responseText&&(e=200),4==d.readyState&&200==e&&(a.outerHTML=d.responseText)};try{d.open("GET",b,!0),d.send()}catch(f){}}var b,c=document.getElementsByTagName("*");for(b in c)c[b].hasAttribute&&c[b].hasAttribute("data-include")&&a(c[b],c[b].getAttribute("data-include"));
 });
 
